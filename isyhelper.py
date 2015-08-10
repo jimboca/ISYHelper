9e60443dcf553f76ff4c17ee8bd9aa4e4186dabe
Jim Searle <jimsearle@gmail.com>
2015-08-10 16:27:52 -0700

    More restructuring

--- isyhelper.py
+++ isyhelper.py
@@ -23,40 +23,9 @@ sys.path.insert(0,"../PyISY")
 sys.path.insert(0,"../VarEvents")
 
 # Load our dependancies
-import logging
-import yaml
-import PyISY
-from isyhelperREST import isyhelperREST
-from Devices import Devices
-from Monitors import Monitors
+import ISYHelper
 
 print('PyISYLink: Started: %s' % datetime.now())
-
-def load_config ():
-    config_file = open('config.yaml', 'r')
-    config = yaml.load(config_file)
-    config_file.close
-    return config
-
-def get_logger(config):
-    if 'log_file' not in config:
-        config['log_file'] = False
-        print("PyISYLink: No log_file defined")
-    elif config['log_file'] == 'none':
-        print("PyISYLink: Not writing log because log_file is none")
-        config['log_file'] = False
-
-    if config['log_file'] != False:
-        print('pylink: Writing to log: ' + config['log_file'])
-        logging.basicConfig(filename=config['log_file']);
-        logger = logging.getLogger()
-        logger.setLevel(0)
-        info = "Startinsg PyISY: host=" + config['isy_host'] + " PORT=" + str(config['isy_port'])
-        logger.info(info)
-    else:
-        logger = False
-    return logger
-
 
 # Load the config file.
 config = load_config();
