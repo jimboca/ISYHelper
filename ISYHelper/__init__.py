969a58a7ddff4f31b8b335a4c08138f422a1f120
Jim Searle <jimsearle@gmail.com>
2015-08-10 18:14:39 -0700

    Added init

---
+++ ISYHelper/__init__.py
@@ -0,0 +1,34 @@
+"""
+Description goes here
+"""
+
+from .Devices import Devices
+from .Monitors import Monitors
+import REST
+
+def load_config ():
+    config_file = open('config.yaml', 'r')
+    config = yaml.load(config_file)
+    config_file.close
+    return config
+
+def get_logger(config):
+    if 'log_file' not in config:
+        config['log_file'] = False
+        print("PyISYLink: No log_file defined")
+    elif config['log_file'] == 'none':
+        print("PyISYLink: Not writing log because log_file is none")
+        config['log_file'] = False
+
+    if config['log_file'] != False:
+        print('pylink: Writing to log: ' + config['log_file'])
+        logging.basicConfig(filename=config['log_file']);
+        logger = logging.getLogger()
+        logger.setLevel(0)
+        info = "Startinsg PyISY: host=" + config['isy_host'] + " PORT=" + str(config['isy_port'])
+        logger.info(info)
+    else:
+        logger = False
+    return logger
+
+
