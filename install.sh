81dda67598396ba364a6ab1d5fcca9936446b41f
jimboca <jimboca3@gmail.com>
2016-02-07 01:07:43 +0000

    Added install and service files

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#git clone https://github.com/jimboca/ISYHelper
+#ISYHelper/install.sh
+
+#sudo apt-get install python-pip
+sudo pip install datetime
+sudo apt-get install libyaml-cpp0.3
+sudo pip install pyaml
+sudo pip install apscheduler
+sudo pip install PyISY
+sudo pip install web.py
+sudo pip install wsgilog
+sudo pip install sleekxmpp
+git clone https://github.com/jimboca/PyISY
+git clone https://github.com/jimboca/fauxmo
+git clone https://github.com/jimboca/hue-upnp
+git clone https://github.com/jimboca/pyharmony
+cd ISYHelper
+
+#cp config.example.yaml config.yaml
+#nano config.yaml
